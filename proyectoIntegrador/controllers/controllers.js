c3a9cb16ab1e86b8779395ff3564adcdf6e968e1
Santiago Revigliono <srevigliono@udesa.edu.ar>
2024-04-09 13:52:35 -0300

    Conecté los métodos al controller.

---
+++ proyectoIntegrador/controllers/controllers.js
@@ -0,0 +1,14 @@
+const products = require("../db/dataBase.js");
+
+const controller = {
+    index: function (req, res) {
+    
+    },
+    product: function (req, res) {
+    }
+   
+
+}
+
+
+module.exports = controller;
--- proyectoIntegrador/routes/products.js
+++ proyectoIntegrador/routes/products.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const productController = require("../controllers/controllers")
 
+router.get("/", productController.index);
+router.get("/:idproducto",productController.product);
 
 
 module.exports = router;
+
+
+

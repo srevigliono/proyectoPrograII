ce8be5aaf705f13a58298bcfc25502b3b246e47e
santiagolegarre <slegarrematera@udesa.edu.ar>
2024-06-10 17:30:00 -0300

    arreglamos register

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 const usersController = require("../controllers/usersControllers");
 const loginValidations = require('../middlewares/login-validator');
+const registerValidation = require('../middlewares/register-validator');
 
 router.get("/login", usersController.login);
-router.post('/', loginValidations, usersController.login)
+router.post('/', loginValidations, usersController.login);
+
+
 router.get("/registrate",usersController.register );
 router.post("/", registerValidation, usersController.store );
 

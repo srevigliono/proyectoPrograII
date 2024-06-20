6ebdd907149666a6d2dc3cec8704dd851201b74c
matias-inda <indam@udesa.edu.ar>
2024-06-20 17:18:28 -0300

    Avance en products controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,39 @@
-const data = require("../database/models");
+const { validationResult } = require('express-validator');
+const db = require("../database/models");
+
 
 const productController = {
     producto:
         function (req ,res) {
-            const productos = data.productos
         res.render("product", {title: "Detalles del producto", productos})
+        const id = req.params.id;
+        const associations = {
+            include: [{associations: "usuario"},{associations: "comments"}]
         }
-    ,
+        const falso = false;
+        
+        db.Product.findByPk(id, criterio)
+        .then(function (results) {
+            if (req.session.user != undefined && req.session.user.id == results.usuario.id) {
+                falso = true
+            }else{
+                return res.render("product", {title:"Productos", products: results, comment: results.comentaris, condition: condition});
+            }
+        }).catch(function (err) {
+            console.log(err);
+        });
+        },
+
     crear:
         function (req ,res) {
-            const usuarios = data.usuarios
-        res.render("product-add", {title: "Agregar productos", usuarios})
-        }
+            if (req.session.user != undefined) {
+                return res.render("product-add", {title: "Agregar productos", usuarios})
+            }else{
+                return res.redirect("/user/login");
+            }
+        },
+
+    
     
 }
 

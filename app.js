881d3587895a8d3227260701fcef324abf9e50e7
Santiago Revigliono <srevigliono@udesa.edu.ar>
2024-06-22 19:24:07 -0300

    Punto 8 y arreglo login.

--- app.js
+++ app.js
@@ -22,9 +22,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/product', productRouter);
 
 app.use(
   session({
@@ -61,6 +58,10 @@ app.use(function(req, res, next) {
     return next();
   }
 });
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/product', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/products.js
+++ routes/products.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 const productController = require("../controllers/productController");
 
+ const productValidation = require('../middlewares/product-validator');
+
 router.get("/detail/:id", productController.producto);
 router.get('/delete/:id', productController.destroy);
 
+router.get("/add",productController.create);
+router.post("/add",productValidation,productController.createStore);
 
 module.exports = router;
 
